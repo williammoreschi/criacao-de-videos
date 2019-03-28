815fde450c64819d7a067fba1dfce23dfe738618
William Moreschi <william.moreschi@guiadomarceneiro.net>
2019-03-28 19:06:10 -0300

    add: pesquisa termo/prefixo.

---
+++ index.js
@@ -0,0 +1,20 @@
+const readline = require('readline-sync');
+function start(){
+	const content = {}
+
+	content.searchTerm = askAndReturnSearchTerm();
+	content.prefix = askAndReturnPrefix();
+	function askAndReturnSearchTerm(){
+		return readline.question('Type a Wikipedia serach term: ');
+	}
+	function askAndReturnPrefix(){
+		const prefixes = ['Who is','What is', 'The history of'];
+		const selectedPrefixIndex =  readline.keyInSelect(prefixes,'Choose one option: ');
+		const selectedPrefixText =  prefixes[selectedPrefixIndex];
+		return selectedPrefixText;
+	}
+	console.log(content);
+}
+
+start();
+
