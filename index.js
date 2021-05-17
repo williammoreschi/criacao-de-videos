e8c568a42c8c4c1fcd149127d13da86343aedf8d
William Moreschi <williammoreschi@gmail.com>
2021-05-16 22:14:18 -0300

    Dar a opçao de buscar no google-trends ou digitar o termo desejado

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const robots = { 
 	prompts:require('./robots/prompts'),
-	input:require('./robots/input.js'),
+	//input:require('./robots/input.js'),
 	text:require('./robots/text.js'),
 	state:require('./robots/state.js'),
 	image:require('./robots/image.js'),
@@ -8,12 +8,12 @@ const robots = {
 	youtube:require('./robots/youtube.js')
 }
 async function start(){
-	//robots.input(); //Pesquisar de forma manual
-	await robots.prompts(); // Devolve os termos do google trends para fazer a pesquisa
+	//robots.input();
+	await robots.prompts();
 	await robots.text();
 	await robots.image();
 	await robots.video();
-	await robots.youtube();
+	//await robots.youtube();
 
 	//const content = robots.state.load();
 	//console.dir(content,{depth:null});
--- robots/prompts.js
+++ robots/prompts.js
@@ -8,11 +8,85 @@ async function robot(){
 		maximumSentences : 7
 	}
 
-  const answers = await askAndReturnAnswers();
+  const otherQuestions = [
+    {
+      type: 'select',
+      name: 'prefix',
+      message: 'Choose one option:',
+      choices: [
+        {
+          title:'Who is',
+          value:'Who is', 
+
+        },
+        {
+          title:'What is',
+          value:'What is', 
+
+        },
+        {
+          title:'The history of',
+          value:'The history of',
+        },
+      ],
+      validate: value => typeof value === 'string' ? value.trim() : false,
+    },
+    {
+      type: 'select',
+      name: 'lang',
+      message: 'Choose Language:',
+      choices: [
+        {title:'pt',value:'pt'}, 
+        {title:'en',value:'en'}, 
+        {title:'es',value:'es'},
+      ],
+      validate: value => typeof value === 'string' ? value.trim() : false,
+    }
+  ]
+
+  const answer = await whichTypeOfSearch(); 
+  
+  let answers = [];
+  switch (answer.type) {
+    case 'google-trends':
+      answers = await askAndReturnAnswersGoogleTrends();
+      break;
+    case 'wikipedia':
+      answers = await askAndReturnAnswersWikipedia();
+      break;
+  }
+  
   state.save(Object.assign(content,answers));
   
-  async function askAndReturnAnswers(){
-    console.log('> askAndReturnAnswers');
+  async function whichTypeOfSearch(){
+    const questions = [
+      {
+        type: 'select',
+        name: 'type',
+        message: 'Type a wikipedia search term or to fetch google-trends:',
+        choices: [
+          {
+            title:'Wikipedia',
+            value:'wikipedia', 
+          },
+          {
+            title:'Google Trends',
+            value:'google-trends', 
+          },
+        ],
+        validate: value => typeof value === 'string' ? value.trim() : false,
+      }
+    ]
+    return new Promise(async (resolve, reject) => {
+      const promptOptions = {
+        onCancel: () => reject(new Error('The user has stopped answering'))
+      }
+      const response = await prompts(questions, promptOptions)
+      resolve(response)
+    });
+  }
+  
+  async function askAndReturnAnswersGoogleTrends(){
     const cnt = 9;
     const questions = [
       {
@@ -21,48 +95,38 @@ async function robot(){
         message: 'Choose one search term:',
         choices: await searchHotTrends(cnt),
         validate: value => typeof value === 'string' ? value.trim() : false,
-      },
-      {
-        type: 'select',
-        name: 'prefix',
-        message: 'Choose one option:',
-        choices: [
-          {
-            title:'Who is',
-            value:'Who is', 
-  
-          },
-          {
-            title:'What is',
-            value:'What is', 
-  
-          },
-          {
-            title:'The history of',
-            value:'The history of',
-          },
-        ],
-        validate: value => typeof value === 'string' ? value.trim() : false,
-      },
-      {
-        type: 'select',
-        name: 'lang',
-        message: 'Choose Language:',
-        choices: [
-          {title:'pt',value:'pt'}, 
-          {title:'en',value:'en'}, 
-          {title:'es',value:'es'},
-        ],
-        validate: value => typeof value === 'string' ? value.trim() : false,
       }
     ];
+    
   
     return new Promise(async (resolve, reject) => {
       const promptOptions = {
         onCancel: () => reject(new Error('The user has stopped answering'))
       }
-      const response = await prompts(questions, promptOptions)
+      const response = await prompts(questions.concat(otherQuestions), promptOptions)
       resolve(response)
+    });
+  }
+  
+  async function askAndReturnAnswersWikipedia(){
+    const questions = [
+      {
+        type: 'text',
+        name: 'searchTerm',
+        message: 'Type a Wikipedia search term:',
+      }
+    ];
+    
+    return new Promise(async (resolve, reject) => {
+      const promptOptions = {
+        onCancel: () => reject(new Error('The user has stopped answering'))
+      }
+      const response = await prompts(questions.concat(otherQuestions), promptOptions);
+      if(response.searchTerm.trim().length){
+        resolve(response);
+      }else{
+        reject(new Error('The user has stopped answering'))
+      }
     });
   }
 
